533cf8b6a7b06a1208f7fff169adbb9139997b20
IvanaSitaric <isitaric@student.tugraz.at>
2020-06-03 21:01:36 +0200

    Test Pin and Unpin Note [KP,SI]

--- Notepad/src/app/Main.java
+++ Notepad/src/app/Main.java
@@ -2,14 +2,10 @@ package app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.*;
 
 public class Main extends Application {
         private NoteView noteView = new NoteView();
@@ -26,7 +22,7 @@ public class Main extends Application {
                 VBox layout = new VBox(12);
                 HBox filesettings = new HBox(10);
                 layout.getChildren().addAll(noteViewGroup);
-                Scene scene = new Scene(layout, 800, 640);
+                Scene scene = new Scene(layout, 1000, 640);
                 scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
                 primaryStage.setScene(scene);
                 primaryStage.setTitle("Notepad");
